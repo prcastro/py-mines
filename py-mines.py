3fb21105e445a1e109d61dc5e4048910677af11e
brk0_0 <brk0_0@server.fake>
2013-12-31 23:48:13 -0200

    First lines added to MineBoard class

--- py-mines.py
+++ py-mines.py
@@ -18,12 +18,73 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import pygame
+import random
 
 class MineBoard():
-	pass
+	def __init__(self, rows, cols, numMines):
+		# Create matrix that indicates hidden spots on the board
+		self.hidden = []
+		for i in range(rows):
+			line = []
+			for j in range(cols):
+				line.append(True)
+			self.hidden.append(line)
+
+		# Initiate a matrix full of zeros that will show the mines
+		# and the numbers
+		self.mines = []
+		for i in range(rows):
+			line = []
+			for j in range(cols):
+				line.append(0)
+			self.mines.append(line)
+
+		# Lists of coordinates
+		minesRows = []
+		minesCols = []
+
+		# Create random coordinates of mines
+		for i in range(numMines):
+			valid = False
+			while not valid:
+				x = random.randint(0, rows - 1)
+				y = random.randint(0, cols - 1)
+				valid = True
+				for i in range(len(minesRows)):
+					if x == minesRows[i] and y == minesCols[i]:
+						valid = False
+			minesRows.append(x)
+			minesCols.append(y)
+
+		# Plot mines in "mines" matrix
+		for i in range(numMines):
+			x = minesRows[i]
+			y = minesCols[i]
+			self.mines[x][y] = -1
+
+		print(self.mines)
+
+	def isHidden(self, x, y):
+		pass
+
+	def isMine (self, x, y):
+		pass
+
+	def drawBoard(self):
+		pass
+
+	def hasWon(self):
+		for x in len(self.hidden):
+			for y in len(self.hidden[0]):
+				if self.hidden[x][y]:
+					return False
+		return True
+
+
 
 def main():
-	pass
+	board = MineBoard(4,5,4)
+
 
 if __name__ == "__main__":
 	main()
