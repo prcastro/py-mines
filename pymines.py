2b27e7e62578d9c744ce14138554a358f0cdc927
brk0_0 <brk0_0@server.fake>
2014-01-05 23:15:30 -0200

    Methods 'openField', 'hasLost', '__gameLost' and others were added. 'drawBoard' method was removed, since, for now on, Pygame will be used only outside the class. The variable 'self.mines' was renamed to 'self.board' and 'self.mines' is now a list of mines coordinates.

--- pymines.py
+++ pymines.py
@@ -32,11 +32,44 @@ class MineBoard():
 	def __init__(self, rows, cols, numMines):
 		"""Initialize an instance."""
 
-		self.__createMinesMatrix(rows, cols, numMines)
-		self.__createHiddenMatrix(rows, cols)
+		self.rows = rows
+		self.cols = cols
 
-		print(self.mines)
-		
+		self.__createBoardMatrix(numMines)
+		self.__createHiddenMatrix()
+
+		print(self.board)
+
+	def openField(self, x, y):
+		"""Open a field on the board"""
+
+		if self.isMine(x, y):
+			self.__gameLost()
+		else:
+			self.hidden[x][y] = False
+			if self.board[x][y] == 0:
+				self.__openSurroundingFields(x, y)
+
+	def hasLost(self):
+		"""Check if the player lost the game.
+
+		This method may be deleted if not useful. This
+		will depend on the game implementation"""
+		for mine in self.mines:
+			x, y = mine
+			if not isHidden(x, y):
+				return True
+
+		return False
+
+	def hasWon(self):
+		"""Check if the player won the game."""
+
+		for x in len(self.hidden):
+			for y in len(self.hidden[0]):
+				if isHidden(x, y) and not isMine(x, y):
+					return False
+		return True
 
 	def isHidden(self, x, y):
 		"""Check if the field at the (x,y) coordinate is hidden.
@@ -50,39 +83,29 @@ class MineBoard():
 
 		Returns True if it's a mine and False otherwise."""
 
-		return self.mines[x][y] == MINE
+		return self.board[x][y] == MINE
 
-	def drawBoard(self):
-		"""Draw the board on screen."""
-
-		pass
-
-	def hasWon(self):
-		"""Check if the player won the game."""
-
-		for x in len(self.hidden):
-			for y in len(self.hidden[0]):
-				if self.hidden[x][y]:
-					return False
-		return True
-
-	def __createMinesMatrix(self, rows, cols, numMines):
-		"""Creates a mines matrix.
+	def __createBoardMatrix(self, numMines):
+		"""Creates a board matrix and mines list.
 
 		Creates matrix with mines in certain fields and numbers
 		on the others. These numbers indicates how many mines
-		are surrounding that field.
+		are surrounding that field. Also creates a list with
+		coordinates tuples, indicating mines coodinates
 		
 		This method is not intended to be used by the client,
 		it's just for internal use by other methods of this
-		class."""
+		class.
 
-		self.mines = []
-		for i in range(rows):
+		minesRows, minesCols and self.mines feels redundant.
+		This method will get a revision after version 1.0."""
+
+		self.board = []
+		for i in range(self.rows):
 			line = []
-			for j in range(cols):
+			for j in range(self.cols):
 				line.append(0)
-			self.mines.append(line)
+			self.board.append(line)
 
 		# Lists of coordinates
 		minesRows = []
@@ -92,8 +115,8 @@ class MineBoard():
 		for i in range(numMines):
 			valid = False
 			while not valid:
-				x = random.randint(0, rows - 1)
-				y = random.randint(0, cols - 1)
+				x = random.randint(0, self.rows - 1)
+				y = random.randint(0, self.cols - 1)
 				valid = True
 				for i in range(len(minesRows)):
 					if x == minesRows[i] and y == minesCols[i]:
@@ -101,19 +124,20 @@ class MineBoard():
 			minesRows.append(x)
 			minesCols.append(y)
 
-		# Plot mines in "mines" matrix
+		# Plot mines in "board" matrix and make mines list
+		self.mines = []
 		for i in range(numMines):
-			x = minesRows[i]
-			y = minesCols[i]
-			self.mines[x][y] = MINE
+			x, y = minesRows[i], minesCols[i]
+			self.board[x][y] = MINE
+			self.mines.append((x, y))
 
 		# Generate the numbers on the board, given the mines
-		for x in range(rows):
-			for y in range(cols):
+		for x in range(self.rows):
+			for y in range(self.cols):
 				if not self.isMine(x, y):
-					self.mines[x][y] = self.__surroundingMines(rows, cols, x, y)
+					self.board[x][y] = self.__surroundingMines(x, y)
 
-	def __createHiddenMatrix(self, rows, cols):
+	def __createHiddenMatrix(self):
 		"""Creates a hidden matrix
 
 		Creates a matrix indicating (with boolean values)
@@ -124,13 +148,13 @@ class MineBoard():
 		class."""
 
 		self.hidden = []
-		for i in range(rows):
+		for i in range(self.rows):
 			line = []
-			for j in range(cols):
+			for j in range(self.cols):
 				line.append(True)
 			self.hidden.append(line)
 
-	def __surroundingMines(self, rows, cols, x, y):
+	def __surroundingMines(self, x, y):
 		"""How many mines surround a field.
 
 		This method considers that field (x,y) isn't a mine.
@@ -144,11 +168,29 @@ class MineBoard():
 		for i in range(-1, 2):
 			for j in range(-1, 2):
 				# Check if x+i and y+j stays inside the board
-				if (x + i >= 0 and x + i < rows) and (y + j >= 0 and y + j < cols):
+				if (x + i >= 0 and x + i < self.rows) and (y + j >= 0 and y + j < self.cols):
 					if self.isMine(x + i, y + j):
 						count += 1
 
 		return count
+
+	def __gameLost(self):
+		"""Take actions after opening a field containing a mine."""
+		self.__openAllFields()
+
+	def __openSurroundingFields(self, x, y):
+		"""Open surrounding fields of a specified cordinate"""
+		for i in range(-1, 2):
+			for j in range(-1, 2):
+				# Check if x+i and y+j stays inside the board
+				if (x + i >= 0 and x + i < self.rows) and (y + j >= 0 and y + j < self.cols):
+					self.openField(x + i, y + j)
+
+	def __openAllFields(self):
+		for i in range(self.rows):
+			for j in range(self.cols):
+				self.hidden[i][j] = False
+
 
 def main():
 	test = MineBoard(4, 4, 4)
