4f30f7f466ff7dc4b544bfee9e01316106e29e48
brk0_0 <brk0_0@server.fake>
2014-01-06 22:40:13 -0200

    Added 'main' and many other functions. Also changed the 'MineBoard' class a bit. Now the game is 'working' but buggy (to say the best). Need various fixes and cleanup.

--- pymines.py
+++ pymines.py
@@ -19,9 +19,41 @@
 
 import pygame
 import random
+from pygame.locals import *
 
-# Constants
+# Pygame related constants
+FPS      = 30
+NUMMINES = 4
+
+BOARDWIDTH   = 4
+BOARDHEIGHT  = 4
+FIELDSIZE = 20
+GAPSIZE = 10
+PANELWIDHT  = int(BOARDWIDTH * (FIELDSIZE + GAPSIZE) - GAPSIZE)
+PANELHEIGHT = 20
+XMARGIN = 3
+YMARGIN = 3
+WINDOWWIDHT  = (2 * XMARGIN) + PANELWIDHT
+WINDOWHEIGHT = (2 * YMARGIN) + PANELHEIGHT + (BOARDHEIGHT * (FIELDSIZE + GAPSIZE))
+MINERADIUS = 5
+
 MINE = -1
+WON  =  1
+LOST = -1
+
+# Colors      R    G    B
+BLACK    = (  0,   0,   0)
+WHITE    = (255, 255, 255)
+RED      = (255,   0,   0)
+BLUE     = (  0,   0, 255)
+GRAY     = (100, 100, 100)
+
+BGCOLOR          = BLACK
+CLOSEDFIELDCOLOR = GRAY
+OPENFIELDCOLOR   = WHITE
+MINEBGCOLOR      = RED
+MINECOLOR        = BLACK
+PANELBGCOLOR     = BLUE
 
 class MineBoard():
 	"""Mine Sweeper board class, used for playing Py-Mines game.
@@ -35,39 +67,32 @@ class MineBoard():
 		self.rows = rows
 		self.cols = cols
 
-		self.__createBoardMatrix(numMines)
+		self.__createMainMatrix(numMines)
 		self.__createHiddenMatrix()
 
-		print(self.board)
-
 	def openField(self, x, y):
-		"""Open a field on the board"""
+		"""Open a field on the board.
+
+		Return LOST if the game a mine was opened,
+		WON if the game was won and 0 otherwise."""
 
 		if self.isMine(x, y):
 			self.__gameLost()
+			return LOST
 		else:
 			self.hidden[x][y] = False
-			if self.board[x][y] == 0:
+			if self.main[x][y] == 0:
 				self.__openSurroundingFields(x, y)
-
-	def hasLost(self):
-		"""Check if the player lost the game.
-
-		This method may be deleted if not useful. This
-		will depend on the game implementation"""
-		for mine in self.mines:
-			x, y = mine
-			if not isHidden(x, y):
-				return True
-
-		return False
+			if self.hasWon():
+				return WON
+		return 0
 
 	def hasWon(self):
 		"""Check if the player won the game."""
 
-		for x in len(self.hidden):
-			for y in len(self.hidden[0]):
-				if isHidden(x, y) and not isMine(x, y):
+		for x in range(self.rows):
+			for y in range(self.cols):
+				if self.isHidden(x, y) and not self.isMine(x, y):
 					return False
 		return True
 
@@ -83,9 +108,9 @@ class MineBoard():
 
 		Returns True if it's a mine and False otherwise."""
 
-		return self.board[x][y] == MINE
+		return self.main[x][y] == MINE
 
-	def __createBoardMatrix(self, numMines):
+	def __createMainMatrix(self, numMines):
 		"""Creates a board matrix and mines list.
 
 		Creates matrix with mines in certain fields and numbers
@@ -100,12 +125,12 @@ class MineBoard():
 		minesRows, minesCols and self.mines feels redundant.
 		This method will get a revision after version 1.0."""
 
-		self.board = []
+		self.main = []
 		for i in range(self.rows):
 			line = []
 			for j in range(self.cols):
 				line.append(0)
-			self.board.append(line)
+			self.main.append(line)
 
 		# Lists of coordinates
 		minesRows = []
@@ -124,18 +149,18 @@ class MineBoard():
 			minesRows.append(x)
 			minesCols.append(y)
 
-		# Plot mines in "board" matrix and make mines list
+		# Plot mines in "main" matrix and make mines list
 		self.mines = []
 		for i in range(numMines):
 			x, y = minesRows[i], minesCols[i]
-			self.board[x][y] = MINE
+			self.main[x][y] = MINE
 			self.mines.append((x, y))
 
 		# Generate the numbers on the board, given the mines
 		for x in range(self.rows):
 			for y in range(self.cols):
 				if not self.isMine(x, y):
-					self.board[x][y] = self.__surroundingMines(x, y)
+					self.main[x][y] = self.__surroundingMines(x, y)
 
 	def __createHiddenMatrix(self):
 		"""Creates a hidden matrix
@@ -193,7 +218,136 @@ class MineBoard():
 
 
 def main():
-	test = MineBoard(4, 4, 4)
+	# Declare global variables
+	global DISPLAY, FONT, PANELFONT, FPSCLOCK
+
+	# Start Pygame
+	pygame.init()
+	FPSCLOCK = pygame.time.Clock()
+	DISPLAY  = pygame.display.set_mode((WINDOWWIDHT, WINDOWHEIGHT))
+	pygame.display.set_caption("Memory Game")
+
+	# Fonts
+	FONT = pygame.font.Font('freesansbold.ttf', 11)
+	PANELFONT = pygame.font.Font('freesansbold.ttf', 11)
+
+	# Start board
+	board = MineBoard(BOARDHEIGHT, BOARDWIDTH, NUMMINES)
+
+	# Print the board on console, for debugging
+	for line in board.main:
+		print(line)
+
+	# Main game loop
+	while True:
+		mouseClicked = False
+		status = 0
+
+		# Event-handling loop
+		for event in pygame.event.get():
+			if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+				terminate()
+			elif event.type == MOUSEBUTTONUP:
+				mouseX, mouseY = event.pos
+				mouseClicked = True
+
+		if mouseClicked:
+			# Get coordinates of the field that the cursor is pointing
+			fieldX, fieldY = getFieldAtPixel(mouseX, mouseY)
+
+			if (fieldX, fieldY) != (None, None):
+				# Open field and store returned status
+				status = board.openField(fieldX, fieldY)
+
+		drawBoard(board, status)
+
+		if status == WON or status == LOST:
+			# If you won or lost the game, it is reseted
+			pygame.display.update()
+			pygame.time.wait(2000)
+			board = MineBoard(BOARDHEIGHT, BOARDWIDTH, NUMMINES)
+
+		# Redraw the screen and wait for the clock tick
+		pygame.display.update()
+		FPSCLOCK.tick(FPS)
+
+def getFieldAtPixel(mouseX, mouseY):
+	"""What field is at pixel's coordinates (x, y)"""
+
+	for x in range(BOARDWIDTH):
+		for y in range(BOARDHEIGHT):
+			left, top = getLeftTopCoordsOfField(x, y)
+			fieldRect = pygame.Rect(left, top, FIELDSIZE, FIELDSIZE)
+
+			# If the point (x, y) collides with this box, then, this is the box under this pixel!
+			if fieldRect.collidepoint(mouseX, mouseY):
+				return (x, y)
+
+	# If no box collides with (x, y)
+	return (None, None)
+
+def getLeftTopCoordsOfField(fieldX, fieldY):
+	left = XMARGIN + (fieldX * (FIELDSIZE + GAPSIZE))
+	top = YMARGIN + (PANELHEIGHT + GAPSIZE) + (fieldY * (FIELDSIZE + GAPSIZE))
+
+	return (left, top)
+
+def drawBoard(board, status):
+	"""Draw the game board on screen."""
+	# Draw background
+	DISPLAY.fill(BGCOLOR)
+
+	# Draw fields
+	for x in range(board.rows):
+		for y in range(board.cols):
+			drawField(board, x, y)
+
+	# Draw the panel
+	drawPanel(board, status)
+
+def drawField(board, x, y):
+	left, top = getLeftTopCoordsOfField(x, y)
+
+	if board.isHidden(x, y):
+		# Draw closed field
+		pygame.draw.rect(DISPLAY, CLOSEDFIELDCOLOR, (left, top, FIELDSIZE, FIELDSIZE))
+	elif board.isMine(x, y):
+		# Draw mine
+		pygame.draw.rect(DISPLAY, MINEBGCOLOR, (left, top, FIELDSIZE, FIELDSIZE))
+		pygame.draw.circle(DISPLAY, MINECOLOR, (int(left + (FIELDSIZE/2)), int(top + (FIELDSIZE/2))), MINERADIUS, 0)
+	elif board.main[x][y] == 0:
+		# Draw opened and empty field
+		pygame.draw.rect(DISPLAY, OPENFIELDCOLOR, (left, top, FIELDSIZE, FIELDSIZE))
+	else:
+		# Draw opened field with a number on it
+		pygame.draw.rect(DISPLAY, OPENFIELDCOLOR, (left, top, FIELDSIZE, FIELDSIZE))
+		textSurfaceObj = FONT.render(str(board.main[x][y]), True, BLACK)
+		textRectObj = textSurfaceObj.get_rect()
+		textRectObj.center = (left + (FIELDSIZE/2), top + (FIELDSIZE/2))
+		DISPLAY.blit(textSurfaceObj, textRectObj)
+
+
+def drawPanel(board, status):
+	"""Draw panel on DISPLAY based on the status. """
+	left, top = XMARGIN, YMARGIN
+
+	pygame.draw.rect(DISPLAY, PANELBGCOLOR, (left, top, PANELWIDHT, PANELHEIGHT))
+
+	if status == WON or status == LOST:
+		# Draw message
+
+		if status == WON:
+			textSurfaceObj = PANELFONT.render("WON!", True, WHITE)
+		else:
+			textSurfaceObj = PANELFONT.render("YOU LOST", True, WHITE)
+
+		textRectObj = textSurfaceObj.get_rect()
+		textRectObj.center = (left + (PANELWIDHT/2), top + (PANELHEIGHT/2))
+		DISPLAY.blit(textSurfaceObj, textRectObj)
+
+def terminate():
+	pygame.quit() # Quit PyGame
+	sys.exit() # Exit program
 
 if __name__ == "__main__":
 	main()
